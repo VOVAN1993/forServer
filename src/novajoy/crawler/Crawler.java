84855735991c94f181b4e6a55bacdd2c41e5fdf4
prikhv <cska631@gmail.com>
2015-05-18 21:35:23 +0300

    init25

--- src/novajoy/crawler/Crawler.java
+++ src/novajoy/crawler/Crawler.java
@@ -17,6 +17,10 @@ import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.XmlReader;
 import novajoy.util.db.JdbcManager;
 import novajoy.util.logger.Loggers;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 
 public class Crawler extends Thread {
     private final JdbcManager dbManager;
@@ -76,7 +80,7 @@ public class Crawler extends Thread {
                     continue;
                 }
 
-                for (Iterator i = feed.getEntries().iterator(); i.hasNext();) {
+                for (Iterator i = feed.getEntries().iterator(); i.hasNext(); ) {
                     if (insert_item((SyndEntry) i.next(),
                             Long.parseLong(rs.getString(1))))
                         ++items_count;
@@ -111,10 +115,10 @@ public class Crawler extends Thread {
         //add reuqest header
         con.setRequestMethod("POST");
         con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-        con.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=windows-1251");
 
         String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
 
+        // Send post request
         con.setDoOutput(true);
         DataOutputStream wr = new DataOutputStream(con.getOutputStream());
         wr.writeBytes(urlParameters);
@@ -135,39 +139,55 @@ public class Crawler extends Thread {
             response.append(inputLine);
         }
         in.close();
-        System.out.println(response.toString());
+
         //print result
         return response.toString();
 
     }
 
-//    private SyndFeed readfeed(String addr) throws Exception {
-//        XmlReader reader = null;
-//        SyndFeed feed1 = null;
-//        try {
-//            String s = sendPost(addr);
-//            InputStream stream = new ByteArrayInputStream(s.getBytes(StandardCharsets.UTF_8));
-//            log.info(s);
-//            reader = new XmlReader(stream);
-//            feed1 = new SyndFeedInput().build(reader);
-//        } finally {
-//            if (reader != null)
-//                reader.close();
-//        }
-//        return feed1;
-//    }
+    public String f(String url) {
+        try {
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpGet request = new HttpGet(url);
+
+            // add request header
+//        request.addHeader("User-Agent", USER_AGENT);
+            HttpResponse response = client.execute(request);
+
+            System.out.println("Response Code : "
+                    + response.getStatusLine().getStatusCode());
+
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+            StringBuffer result = new StringBuffer();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            return result.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     private SyndFeed readfeed(String addr) throws Exception {
         XmlReader reader = null;
-        SyndFeed feed = null;
+        SyndFeed feed1 = null;
         try {
-            reader = new XmlReader(new URL(addr));
-            feed = new SyndFeedInput().build(reader);
+
+            String s = f(addr);
+            System.out.println(s);
+            InputStream stream = new ByteArrayInputStream(s.getBytes(StandardCharsets.UTF_8));
+            log.info(s);
+            reader = new XmlReader(stream);
+            feed1 = new SyndFeedInput().build(reader);
         } finally {
             if (reader != null)
                 reader.close();
         }
-        return feed;
+        return feed1;
     }
 
     private boolean insert_item(SyndEntry entry, long feedid)
