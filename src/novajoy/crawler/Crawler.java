8ac85e6087db9dff8dd62aba2e757b7f86f67c7f
prikhv <cska631@gmail.com>
2015-05-18 22:06:22 +0300

    init28

--- src/novajoy/crawler/Crawler.java
+++ src/novajoy/crawler/Crawler.java
@@ -17,13 +17,14 @@ import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.XmlReader;
 import novajoy.util.db.JdbcManager;
 import novajoy.util.logger.Loggers;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpVersion;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 
 public class Crawler extends Thread {
     private final JdbcManager dbManager;
@@ -151,8 +152,6 @@ public class Crawler extends Thread {
     public String f(String url) {
         try {
             HttpClient client = HttpClientBuilder.create().build();
-            client.getParams().setParameter("http.protocol.version", HttpVersion.HTTP_1_1);
-            client.getParams().setParameter("http.protocol.content-charset", "UTF-8");
             HttpGet request = new HttpGet(url);
 
             // add request header
@@ -161,16 +160,10 @@ public class Crawler extends Thread {
 
             System.out.println("Response Code : "
                     + response.getStatusLine().getStatusCode());
-
-            BufferedReader rd = new BufferedReader(
-                    new InputStreamReader(response.getEntity().getContent(),"UTF-8"));
-
-            StringBuffer result = new StringBuffer();
-            String line = "";
-            while ((line = rd.readLine()) != null) {
-                result.append(line);
-            }
-            return result.toString();
+            HttpEntity entity = response.getEntity();
+            String result = EntityUtils.toString(entity, "UTF-8");
+            System.out.println(result);
+            return result;
         } catch (Exception e) {
             e.printStackTrace();
         }
